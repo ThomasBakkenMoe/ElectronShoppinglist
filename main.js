e4bf9a166c10ffcb5ee1f6a40f1384ad3dcd6eac
Thomas Bakken Moe <thomasbakkenmoe@gmail.com>
2019-12-14 17:56:38 +0100

    Added "Add new Item"-window

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
 
@@ -12,17 +13,46 @@ app.on('ready', ()=>{
     // Create new window
     mainWindow = new BrowserWindow({}); // Parameter object is as of yet empty, as there is no configuration options that we need to pass in.
     // Load html into window
+
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'), // dirname/mainWindow.html
         protocol: 'file:', // file:
         slashes: true // //
     })); //What this urlObject is doing: It constructs the path "file://dirname/mainWindow.html" and passes it into the loadURL function.
 
+    //Quit application when closed
+    mainWindow.on('closed', ()=>{
+       app.quit();
+    });
+
     // Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     // Insert menu
     Menu.setApplicationMenu(mainMenu)
 });
+
+// Handle createAddWindow()
+function createAddWindow(){
+    // Create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+
+    // Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'), // dirname/addWindow.html
+        protocol: 'file:', // file:
+        slashes: true // //
+    })); //What this urlObject is doing: It constructs the path "file://dirname/addWindow.html" and passes it into the loadURL function.
+
+    //Garbage collection handle
+    addWindow.on('close', ()=>{
+        addWindow = null; //Removes the addWindow from memory.
+    })
+
+}
 
 // Create menu template
 const mainMenuTemplate = [
@@ -31,7 +61,9 @@ const mainMenuTemplate = [
         submenu:[
             {
                 label: 'Add Item',
-
+                click() {
+                    createAddWindow();
+                }
             },
             {
                 label: 'Clear Items'
